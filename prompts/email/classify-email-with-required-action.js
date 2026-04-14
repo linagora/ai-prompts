61f4a79b1d34247019e1f9c49a2145f9461f55bf
amghirbi <amghirbi@linagora.com>
2026-04-14 18:23:08 +0200

    feat: keeping old prompt logic with yes or no field for action required  in addition to generic label based  prompt

---
+++ prompts/email/classify-email-with-required-action.js
@@ -0,0 +1,28 @@
+// const { buildMessages } = require('../../utils/prompts');
+// const { EMAIL_SYSTEM_INSTRUCTIONS_WITH_ACTION_FIELD } = require('../system_prompts');
+// const { classificationFormatValid, actionRequirementCorrect, labelAccuracyScore} = require('../../utils/assertions');
+// const { emailTestCases, formatEmailForClassification } = require('../../datasets/email-classification-testcases');
+
+// const task = 'Classify the email to determine if it requires action and assign relevant labels.';
+
+// module.exports = {
+//   id: 'classify-email',
+//   description: 'Classify email for action requirement and assign relevant labels',
+//   version: '1.0.0',
+//   messages: buildMessages({ 
+//     task, 
+//     system_instruction: EMAIL_SYSTEM_INSTRUCTIONS_WITH_ACTION_FIELD 
+//   }),
+  
+//   tests: emailTestCases.map(testCase => ({
+//     description: testCase.description,
+//     vars: {
+//       input: formatEmailForClassification(testCase.input)
+//     },
+//     assert: [
+//       classificationFormatValid(),
+//       actionRequirementCorrect(testCase.expectedOutput.action),
+//       labelAccuracyScore(testCase.expectedOutput.labels)
+//     ]
+//   }))
+// };
